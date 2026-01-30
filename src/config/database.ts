d18d1f89ed5c696a979c41567b5fab3039182841
Breno <breno.soriani@gmail.com>
2026-01-29 23:16:22 -0300

    feat: initialize TypeORM data source for MySQL database connection.

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DATABASE_NAME || 'stock_savvy',
   synchronize: process.env.NODE_ENV !== 'production',
   logging: false,
-  entities: [process.env.NODE_ENV === 'production' ? 'dist/entities/**/*.js' : 'src/entities/**/*.ts'],
+  entities: ['src/entities/**/*.ts'],
   migrations: [process.env.NODE_ENV === 'production' ? 'dist/migrations/**/*.js' : 'src/migrations/**/*.ts'],
   subscribers: []
 });
